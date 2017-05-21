33d06c547d3163056ffd18f95184e55a6177e45e
Michael Diego <michhdiego@gmail.com>
2017-05-21 00:12:53 -0300

    Add Redux basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './redux';
+
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Counter />
       </div>
     );
   }
