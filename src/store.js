3aaff706658bb600aae923dfee0ea32595fc9c1a
Michael Diego <michhdiego@gmail.com>
2017-05-22 02:22:17 -0300

    Add redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
 import reducers from './reducers';
 
 const store = createStore(
-  reducers
+  reducers,
+  applyMiddleware(
+    thunkMiddleware
+  )
 );
 
 export default store;
