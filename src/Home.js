9cc2314c3cbf31be4b68c462a559f959d05ed885
Michael Diego <michhdiego@gmail.com>
2017-05-22 23:11:50 -0300

    Add map

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,28 @@ import { connect } from 'react-redux';
 
 import store from './store';
 
+const MIN_MAG = 2.5;
+
+const isToday = (firstDate, secondDate) => {
+  return firstDate.setHours(0,0,0,0) ===
+         secondDate.setHours(0,0,0,0);
+}
+
+const getEarthquakes = (earthquakes, filter) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return earthquakes;
+    case 'SHOW_LATEST_SIGNIFICANT':
+      return earthquakes.filter(function(earthquake, index) {
+        const today = new Date();
+        const date = new Date(earthquake.properties.time);
+        return earthquake.properties.mag >= MIN_MAG && isToday(today, date);
+      });
+    default:
+      return earthquakes;
+  }
+}
+
 const mapStateToProps = (state, params) => {
   return {
     data: store.getState().data
@@ -10,11 +32,26 @@ const mapStateToProps = (state, params) => {
 };
 
 class Home extends Component {
-  render() {
+
+  renderMap() {
     const { data } = this.props.data;
-    console.log(data);
+    if (!data.features) {
+      return null;
+    }
+    const earthquakes = getEarthquakes(
+      data.features,
+      'SHOW_LATEST_SIGNIFICANT'
+    );
+    earthquakes.map(function(earthquake, index) {
+      const date = new Date(earthquake.properties.time);
+      console.log(date);
+    });
+  }
+
+  render() {
     return (
       <div>
+        {this.renderMap()}
         Home
       </div>
     );
