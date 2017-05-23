f47cee2ef3600c06c6980048225b6eb187306636
Michael Diego <michhdiego@gmail.com>
2017-05-23 05:01:50 -0300

    Hover style to MapMark

--- src/components/MapMark.js
+++ src/components/MapMark.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import cn from 'classnames';
 
 class MapMark extends Component {
+  static propTypes = {
+    // GoogleMap pass $hover props to hovered components
+    // to detect hover it uses internal mechanism, explained in x_distance_hover example
+    $hover: PropTypes.bool,
+    text: PropTypes.string,
+    magnitude: PropTypes.number
+  };
+
   render() {
     const { text, magnitude } = this.props;
     const markClassName = cn(
       'c-map__mark',
-      magnitude >= 4.5 ? 'c-map__mark--alert' : ''
+      magnitude >= 4.5 ? 'c-map__mark--alert' : '',
+      this.props.$hover ? 'c-map__mark--hover' : ''
     );
     return (
       <img
